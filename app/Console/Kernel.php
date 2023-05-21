2ee449465ed1cefcefef87b418c205f6f88ca9cd
Shiina <shiinayusuke01@gmail.com>
2023-05-21 20:55:17 +0900

    supoyaro:checkをスケジュール登録

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('supoyaro:check')->dailyAt('22:00')->appendOutputTo('supoyaro.log');
     }
 
     /**
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
